df3919283e26a24c3c5042840a7f7a8a5ccc3ee0
ChristianMalki <90771003+ChristianMalki@users.noreply.github.com>
2022-11-10 22:48:17 +0100

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Christian Malki on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
